e2e99aa296f9041d0c3f107a29f5fc29685d04a7
leang <leangk49@gmail.com>
2024-03-06 10:26:20 +0700

    basic authentication with laravel breeze

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
+
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
@@ -22,7 +23,9 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
-
+    protected $routeMiddleware = [
+        'auth.check'=>\App\Http\Middleware\AdminCheck::class,
+    ];
     /**
      * The application's route middleware groups.
      *
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,28 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/person/{id?}',function($dog){
-    return $dog;
-});
-Route::get('/animals',function(){
-    return 'dog and cat';
-})->name('animals');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::group(['as'=>'animals.','prefix'=>'/animals'],function(){
-    Route::get('dog',function(){
-        return 'Woof';
-    })->name('dog');
-    Route::get('cat',function(){
-        return 'meow';
-    })->name('cat');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('home',function(){
-    return view('home');
-})->name('home');
-Route::get('contact',function(){
-    return view('contact');
-})->name('contact');
-
-
-Route::resource('blog', BlogController::class);
+require __DIR__.'/auth.php';
